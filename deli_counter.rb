6335d17ad488a764d88453d2636c9a8819499536
Sean Langton <slangton1@gmail.com>
2018-10-27 03:03:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,11 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli = [] then puts "There is nobody waiting to be served!"
+    else serving = katz_deli.shift
+         puts "Currently serving #{serving}."
+  end        
+end
