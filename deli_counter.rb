69c976884bcb12e17ae4410edf4f3c28329c3a91
Sean Langton <slangton1@gmail.com>
2018-10-27 03:00:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli = [] then puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
