4dc1c87ebca70be8364d47c4f12d31d37bd68ef9
Sean Langton <slangton1@gmail.com>
2018-10-27 03:09:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,14 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli = [] then puts "The line is currently empty."
+  else
+    line_size = katz_deli.size
+    current_customer = 0
+    line_order = ""
+    while current_customer < line_size do
+      line_order << "#{current_customer+1}. #{katz_deli[current_customer]}"
+    end
+    return "The line is currently: #{line_order}"
   end
 end
 
@@ -15,5 +23,5 @@ def now_serving(katz_deli)
   if katz_deli = [] then puts "There is nobody waiting to be served!"
     else serving = katz_deli.shift
          puts "Currently serving #{serving}."
-  end        
+  end
 end
